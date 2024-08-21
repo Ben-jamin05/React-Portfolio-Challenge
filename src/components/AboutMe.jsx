30350177d32fa7d4d6d90c941fca27b2188be9e8
Ben Hill <benjaminjhill@icloud.com>
2024-08-20 21:38:48 -0500

    changed image locations

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,7 @@
 const AboutMe = () => {
     return (
       <section>
-        <img src="./src/assets/images/totally-me.jpeg" alt="Developer" />
+        <img src="images/totally-me.jpeg" alt="Developer" />
         <p>I know how to code pretty well. I really dont know what else to say I will add to this soon.</p>
       </section>
     );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,37 +5,37 @@ const Portfolio = () => {
   const projects = [
     {
       title: 'Dad-abase',
-      image: './src/assets/images/dad-abase.jpeg',
+      image: 'images/dad-abase.jpeg',
       deployLink: 'https://dad-abase.onrender.com',
       repoLink: 'https://github.com/JohnKihm/Dad-abase',
     },
     {
       title: 'Cinema Quiz',
-      image: './src/assets/images/cinema-quiz.jpeg',
+      image: 'images/cinema-quiz.jpeg',
       deployLink: 'https://excervantes.github.io/cinema-quiz/',
       repoLink: 'https://github.com/EXCervantes/cinema-quiz',
     },
     {
       title: 'PWA Text Editor',
-      image: './src/assets/images/PWA-text-editor.jpeg',
+      image: 'images/PWA-text-editor.jpeg',
       deployLink: 'https://pwa-text-editor-challenge-in96.onrender.com',
       repoLink: 'https://github.com/Ben-jamin05/PWA-Text-Editor-Challenge',
     },
     {
       title: 'README Generator',
-      image: './src/assets/images/README-generator.jpeg',
+      image: 'images/README-generator.jpeg',
       deployLink: 'https://github.com/Ben-jamin05/README-generator-challenge',
       repoLink: 'https://github.com/Ben-jamin05/README-generator-challenge',
     },
     {
       title: 'Social Network API',
-      image: './src/assets/images/social-network-api.jpeg',
+      image: 'images/social-network-api.jpeg',
       deployLink: 'https://github.com/Ben-jamin05/Social-Network-API-Challenge',
       repoLink: 'https://github.com/Ben-jamin05/Social-Network-API-Challenge',
     },
     {
       title: 'Note Taker',
-      image: './src/assets/images/note-taker.jpeg',
+      image: 'images/note-taker.jpeg',
       deployLink: 'https://note-taker-challenge-kn1o.onrender.com',
       repoLink: 'https://github.com/Ben-jamin05/Note-Taker-Challenge',
     },
